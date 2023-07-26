33816dd4ab2c8a6ee2b05d94975f052d3b087941
Alexey Kolodin <alekko-sp@yandex.ru>
2023-07-26 23:06:49 +0300

    there is the homework in this branch

--- src/main/resources/liquibase/scripts/course-four-lesson-three.sql
+++ src/main/resources/liquibase/scripts/course-four-lesson-three.sql
@@ -5,11 +5,21 @@ CREATE INDEX students_name_index ON student (name);
 
 -- changeset akolodin:2
 ALTER TABLE faculty
-ADD COLUMN name_faculty TEXT;
+    ADD COLUMN name_faculty TEXT;
 
 -- changeset akolodin:3
 CREATE INDEX faculty_name_coor ON faculty (color, name_faculty);
 
+-- changeset akolodin:4
+DROP INDEX faculty_name_coor;
+
+-- changeset akolodin:5
+ALTER TABLE faculty
+DROP
+COLUMN name_faculty;
+
+-- changeset akolodin:6
+CREATE INDEX faculty_color_name ON faculty (color, name);
 
 
 
